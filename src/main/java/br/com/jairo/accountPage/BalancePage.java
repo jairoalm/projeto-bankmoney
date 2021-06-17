2d0fe039bfc51e225668de6941d2c1f5b4fa4249
Jairo Almeida <jairoam85@gmail.com>
2021-06-16 22:18:57 -0300

    Class review

--- src/main/java/br/com/jairo/accountPage/BalancePage.java
+++ src/main/java/br/com/jairo/accountPage/BalancePage.java
@@ -1,13 +1,12 @@
 package br.com.jairo.accountPage;
 
 import org.openqa.selenium.By;
+import br.com.jairo.page.BasePage;
 
-import br.com.jairo.config.DriverFatory;
-
-public class BalancePage {
+public class BalancePage extends BasePage{
 	
 	public String validateAccountBalance(){
-		return DriverFatory.getDriver().findElement(By.xpath("//strong[2]")).getText();
+		return getText(By.xpath("//strong[2]"));
 	}
 
 }

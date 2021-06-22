e3397276c0065731002fe5f1d9c09e94d795d1d8
Jairo Almeida <jairoam85@gmail.com>
2021-06-21 22:14:22 -0300

    Changing method structure to page object pattern

--- src/main/java/br/com/jairo/accountPage/WithdrawalPage.java
+++ src/main/java/br/com/jairo/accountPage/WithdrawalPage.java
@@ -4,37 +4,35 @@ import org.openqa.selenium.By;
 /*import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;*/
 
-import br.com.jairo.config.DriverFatory;
 import br.com.jairo.page.BasePage;
 
 public class WithdrawalPage extends BasePage {
 
-	DepositPage valorDepositado = new DepositPage();
-
 	public void withdrawButton() {
-		DriverFatory.getDriver().findElement(By.xpath("//button[contains(.,'Withdrawl')]")).click();
+		clickButton(By.xpath("//button[contains(.,'Withdrawl')]"));
 	}
 
-	public void withdrawalAmount(String value) throws InterruptedException {
+	/* Make withdrawal */
+	public void setWithdrawalValue(String value) throws InterruptedException {
 		Thread.sleep(2000);
 		/*
 		 * WebDriverWait wait = new WebDriverWait(DriverFatory.getDriver(), 60);
 		 * wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
 		 * "//input[@type='number']")));
 		 */
-		DriverFatory.getDriver().findElement(By.xpath("//input[@type='number']")).sendKeys(value);
+         write(By.xpath("//input[@type='number']"), value);
     }
 
 	public void saveValueWithdrawal() {
-		DriverFatory.getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+		clickButton(By.xpath("//button[@type='submit']"));
 	}
 
 	public String successfulWithdrawal() {
-		return obterTexto(By.xpath("//span[contains(.,'Transaction successful')]"));
+		return getText(By.xpath("//span[contains(.,'Transaction successful')]"));
 	}
 
 	public String errorWithdrawing() {
-		return obterTexto(By
+		return getText(By
 				.xpath("//span[contains(.,'Transaction Failed. You can not withdraw amount more than the balance.')]"));
 	}
 
